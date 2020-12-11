fefba28b8c28dd7f9440dbabad0c60f9b39b0b05
gsaegom <guillermosaezgomez@gmail.com>
2020-12-11 11:43:12 +0100

    Story 05: Front-end. Actually added the BecomeACoach component.

---
+++ src/app/become-a-coach/become-a-coach.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BecomeACoachComponent } from './become-a-coach.component';
+
+describe('BecomeACoachComponent', () => {
+  let component: BecomeACoachComponent;
+  let fixture: ComponentFixture<BecomeACoachComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BecomeACoachComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BecomeACoachComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/become-a-coach/become-a-coach.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-become-a-coach',
+  templateUrl: './become-a-coach.component.html',
+  styleUrls: ['./become-a-coach.component.css']
+})
+export class BecomeACoachComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
